6dcd7b8a05031d2748e693b17ef10765a9713473
William Ko <william.ko@boeing.com>
2021-06-07 14:34:39 +1000

    Pose3D multiplication order changed in ignition math

--- src/gazebo_model_attachment_plugin.cpp
+++ src/gazebo_model_attachment_plugin.cpp
@@ -170,7 +170,7 @@ void ModelAttachmentPlugin::attach(const std::string& joint_name, physics::Model
     ignition::math::Pose3d m1wp = m1->WorldPose();
     ignition::math::Pose3d l1rl = l1->RelativePose();
     ignition::math::Pose3d l2rl = l2->RelativePose();
-    ignition::math::Pose3d p = (l2rl.Inverse() * l1rl * m1wp);
+    ignition::math::Pose3d p = (m1wp * l1rl * l2rl.Inverse());
 
     m2->SetWorldPose(p);
 
